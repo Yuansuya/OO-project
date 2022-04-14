9cd3e3a6e0b11f0f1a5756d6045aa1074b137812
Yuansuya <kevincolin933@gmail.com>
2022-04-14 23:20:09 +0800

    add determination for Connector show

--- src/class_shape.java
+++ src/class_shape.java
@@ -4,23 +4,21 @@ import java.awt.*;
 import MyShape.*;
 public class class_shape extends MyShape
 {
-	private int shape_h =90;
-	private int shape_w =90;
 	public class_shape(Point SP)
 	{
-		super(SP);
-		super.EndPoint = new Point(SP.x + shape_h, SP.y + shape_w);
-		super.DirPoints[0] = new Point(SP.x + shape_h / 2 - super.offset, SP.y - super.offset);
-		super.DirPoints[1] = new Point(SP.x + shape_h - super.offset, SP.y + shape_w /2 - super.offset);
-		super.DirPoints[2] = new Point(SP.x + shape_h / 2- super.offset, SP.y + shape_w- super.offset);
-		super.DirPoints[3] = new Point(SP.x - super.offset, SP.y + shape_w /2 - super.offset);		
+		super(SP);	
 	}
 	public void DrawShape(Graphics g)
 	{
-		int part =shape_h /3 ;
-		g.drawRect(super.StartPoint.x, super.StartPoint.y, shape_h,shape_w);
-		g.drawLine(super.StartPoint.x, super.StartPoint.y + part, super.StartPoint.x + shape_w, super.StartPoint.y + part);
-		g.drawLine(super.StartPoint.x, super.StartPoint.y + part*2, super.StartPoint.x + shape_w, super.StartPoint.y + part*2);
+		int part =super.height /3 ;
+		g.drawRect(super.Corners[0].x, super.Corners[0].y, super.height,super.width);
+		g.drawLine(super.Corners[0].x, super.Corners[0].y + part, super.Corners[0].x + super.width, super.Corners[0].y + part);
+		g.drawLine(super.Corners[0].x, super.Corners[0].y + part*2, super.Corners[0].x + super.width, super.Corners[0].y + part*2);
+		
+		if(super.IsConnectorShow)
+		{
+			super.DrawCorners(g);
+		}
 	}
 	
 	
