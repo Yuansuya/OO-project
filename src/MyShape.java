4c7a9ceb32d4c8e70764363603b4c81cab60a670
Yuansuya <kevincolin933@gmail.com>
2022-04-17 02:00:35 +0800

    加上判斷滑鼠是否在圖內的function

--- src/MyShape.java
+++ src/MyShape.java
@@ -5,19 +5,20 @@ import java.util.*;
 public class MyShape
 {
 	protected Point[] Corners = new Point[4]; // TopLeft, TopRight, DownRight, DownLeft
-	protected final int width = 100;
-	protected final int height = (int)(width*1.5);
+	protected final int height = 100;
+	protected final int width= (int)(height*1.5);
 	protected Point[] Connectors = new Point[4]; //East, south, west, north
 	protected String name;
 	protected boolean IsNameShow = false;
 	protected boolean IsConnectorShow = false;
-	final int offset = 5;
+	protected int depth ;
+	private final int offset = 5;
 	public MyShape(Point SP)
 	{
 		this.Corners[0] = SP;
-		this.Corners[1] = new Point(SP.x, SP.y + width);
-		this.Corners[2] = new Point(SP.x + width, SP.y);
-		this.Corners[3] = new Point(SP.x + width, SP.y + height);
+		this.Corners[1] = new Point(SP.x + width, SP.y);
+		this.Corners[2] = new Point(SP.x + width, SP.y + height);
+		this.Corners[3] = new Point(SP.x, SP.y + height);
 		
 		this.Connectors[0] = new Point(SP.x + width/2, SP.y);
 		this.Connectors[1] = new Point(SP.x + width, SP.y + height/2);
@@ -48,18 +49,39 @@ public class MyShape
 		this.Corners[2] = new Point(down, right);
 		this.Corners[3] = new Point(down, left);
 	}
-	protected void DrawShape(Graphics g)
+	public void DrawShape(Graphics g)
 	{
 		;
 	}
 	
-	protected void DrawCorners(Graphics g)
+	public void DrawConnectors(Graphics g)
 	{
 		g.setColor(Color.BLACK);
 		for(int i  =0 ;i < 4; i ++)
 		{				
-			g.fillOval(this.Connectors[i].x, this.Connectors[i].y, 10, 10);
+			g.fillOval(this.Connectors[i].x - this.offset, this.Connectors[i].y - this.offset, 10, 10);
 		}
 	}
 	
+	public void setConnectorShow(boolean b)
+	{
+		this.IsConnectorShow = b;
+	}
+	
+	public boolean getConnectorShow(MyShape s)
+	{
+		return IsConnectorShow;
+	}
+	
+	public boolean IsMouseInShape(Point CurrentPoint)
+	{
+		int x_axis = CurrentPoint.x - this.Corners[0].x;
+		if(x_axis < 0 || x_axis > this.width)
+			return false;
+		int y_axis = CurrentPoint.y - this.Corners[0].y;
+		if(y_axis < 0 || y_axis > this.height)
+			return false;
+		return true;
+		
+	}
 }
