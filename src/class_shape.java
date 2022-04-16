b264ed146fb25b4d8b6e568582bfe3ffd5fa8d22
Yuansuya <kevincolin933@gmail.com>
2022-04-17 02:02:06 +0800

    修改height, weight丟反的bug

--- src/class_shape.java
+++ src/class_shape.java
@@ -11,13 +11,13 @@ public class class_shape extends MyShape
 	public void DrawShape(Graphics g)
 	{
 		int part =super.height /3 ;
-		g.drawRect(super.Corners[0].x, super.Corners[0].y, super.height,super.width);
+		g.drawRect(super.Corners[0].x, super.Corners[0].y, super.width,super.height);
 		g.drawLine(super.Corners[0].x, super.Corners[0].y + part, super.Corners[0].x + super.width, super.Corners[0].y + part);
 		g.drawLine(super.Corners[0].x, super.Corners[0].y + part*2, super.Corners[0].x + super.width, super.Corners[0].y + part*2);
 		
 		if(super.IsConnectorShow)
 		{
-			super.DrawCorners(g);
+			super.DrawConnectors(g);
 		}
 	}
 	
