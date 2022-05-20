074306180636877e7a9cce165b7aebab9816e9fc
Yuansuya <kevincolin933@gmail.com>
2022-05-20 19:02:30 +0800

    rename Draw and introducing the composite design pattern

--- src/MyShape.java
+++ src/MyShape.java
@@ -1,18 +1,24 @@
-package MyShape;
+package MyGraphic;
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
-public class MyShape
+import MyGraphic.*;
+public class MyShape extends MyGraphic 
 {
 	protected Point[] Corners = new Point[4]; // TopLeft, TopRight, DownRight, DownLeft
-	protected final int height = 100;
-	protected final int width= (int)(height*1.5);
+	protected int height = 100;
+	protected int width= (int)(height*1.5);
 	public Point[] Connectors = new Point[4]; //East, south, west, north
 	protected String name = "";
 	protected boolean IsNameShow = false;
 	protected boolean IsConnectorShow = false;
 	protected int depth;
 	private final int offset = 5;
+	private boolean Grouped = false ;
+	public MyShape()
+	{
+		
+	}
 	public MyShape(Point SP)
 	{
 		this.Corners[0] = SP;
@@ -25,36 +31,14 @@ public class MyShape
 		this.Connectors[2] = new Point(SP.x + width/2, SP.y + height);
 		this.Connectors[3] = new Point(SP.x, SP.y + height/2);
 	}
-	//for group 
-	public MyShape(MyShape[] shapes)
-	{
-		int top=0;
-		int down=0;
-		int left=0;
-		int right=0;
-		for(MyShape sh: shapes)
-		{
-			if(top > sh.Corners[0].y)
-				top = sh.Corners[0].y ;
-			if(left > sh.Corners[0].x)
-				left =sh.Corners[0].y ;
-			if(right < sh.Corners[2].x)
-				right = sh.Corners[2].x ;
-			if(down < sh.Corners[2].x)
-				down = sh.Corners[2].x ;
-		}
-		
-		this.Corners[0] = new Point(top, left);
-		this.Corners[1] = new Point(top, right);
-		this.Corners[2] = new Point(down, right);
-		this.Corners[3] = new Point(down, left);
-	}
-	public void DrawShape(Graphics g)
+
+
+	public void Draw(Graphics g)
 	{
 		;
 	}
 	
-	public void DrawConnectors(Graphics g)
+	protected void DrawConnectors(Graphics g)
 	{
 		g.setColor(Color.BLACK);
 		for(int i  =0 ;i < 4; i ++)
@@ -65,12 +49,21 @@ public class MyShape
 	
 	public void setConnectorShow(boolean b)
 	{
-		this.IsConnectorShow = b;
+		if(!Grouped)
+			this.IsConnectorShow = b;
 	}
 	
 	public boolean getConnectorShow()
 	{
 		return IsConnectorShow;
+	}
+	
+	public boolean IsShapeInArea(Point TopLeft, Point DownRight)
+	{
+		if(Corners[0].x >= TopLeft.x && Corners[0].y >= TopLeft.y && Corners[2].x <= DownRight.x && Corners[2].y <= DownRight.y)
+			return true;
+		else
+			return false;
 	}
 	
 	// @param CurrentPoint refers to where the mouse point now
@@ -117,21 +110,6 @@ public class MyShape
 		return this.Connectors;
 	}
 	
-	public void setCornersLocation(Point SP)
-	{
-		this.Corners[0] = SP;
-		this.Corners[1] = new Point(SP.x + width, SP.y);
-		this.Corners[2] = new Point(SP.x + width, SP.y + height);
-		this.Corners[3] = new Point(SP.x, SP.y + height);
-	}
-	public void setConnectorsLocation(Point SP)
-	{
-		this.Connectors[0] = new Point(SP.x + width/2, SP.y);
-		this.Connectors[1] = new Point(SP.x + width, SP.y + height/2);
-		this.Connectors[2] = new Point(SP.x + width/2, SP.y + height);
-		this.Connectors[3] = new Point(SP.x, SP.y + height/2);
-	}
-	
 	public Point[] getCorners()
 	{
 		return this.Corners;
@@ -140,5 +118,35 @@ public class MyShape
 	public void setName(String name)
 	{
 		this.name = name;
+	}
+	
+	
+	
+	public void move(int offset_x, int offset_y)
+	{
+		this.Corners[0] = new Point(Corners[0].x += offset_x, Corners[0].y += offset_y);
+		this.Corners[1] = new Point(Corners[1].x += offset_x, Corners[1].y += offset_y);
+		this.Corners[2] = new Point(Corners[2].x += offset_x, Corners[2].y += offset_y);
+		this.Corners[3] = new Point(Corners[3].x += offset_x, Corners[3].y += offset_y);
+		
+		this.Connectors[0] = new Point(Connectors[0].x +=offset_x, Connectors[0].y += offset_y);
+		this.Connectors[1] = new Point(Connectors[1].x +=offset_x, Connectors[1].y += offset_y);
+		this.Connectors[2] = new Point(Connectors[2].x +=offset_x, Connectors[2].y += offset_y);
+		this.Connectors[3] = new Point(Connectors[3].x +=offset_x, Connectors[3].y += offset_y);
+	}
+	
+	public void setGrouped(boolean b)
+	{
+		this.Grouped = b ; 
+	}
+	public boolean getGrouped()
+	{
+		return this.Grouped; 
+	}
+	
+	//@return int[4] = {min_x, min_y, max_x, max_y}
+	public int[] getBorders()
+	{
+		return new int[]{this.Corners[0].x, this.Corners[0].y, this.Corners[2].x, this.Corners[2].y};
 	}
 }
