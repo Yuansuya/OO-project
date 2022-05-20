fa92729a67810c31da4a2e950254382c28e8b0db
Yuansuya <kevincolin933@gmail.com>
2022-05-20 19:05:44 +0800

    A base class for ploymorphism for composite design pattern

---
+++ src/MyGraphic.java
@@ -0,0 +1,10 @@
+package MyGraphic;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.*;
+
+public abstract class MyGraphic
+{
+	public abstract void Draw(Graphics g);
+}
