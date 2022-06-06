bbcc9cd6a7c8bd801f921124d7cd7d51d9382264
Yuansuya <kevincolin933@gmail.com>
2022-06-06 16:29:53 +0800

    nothing

--- src/Mode.java
+++ src/Mode.java
@@ -9,7 +9,6 @@ public abstract class Mode
 	public Mode(MyGraphic gg)
 	{
 		this.graphics = (GroupGraphic)gg ;
-		System.out.println("Test");
 	}
 	public abstract void ClickAction(Point CurrentPoint,int depth_counter);
 	public abstract void DragAction(Point CurrentPoint);
